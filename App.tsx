faa92245fca627452627af38fdc205c093e3e225
Jorge Henrique <jorgehenriquedasilva2014@hotmail.com>
2022-07-31 15:49:00 -0300

    button component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Button } from './src/components/Button'
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Git Branch Develop</Text>
+      <Button />
       <StatusBar style="auto" />
     </View>
   );
