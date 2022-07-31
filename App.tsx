38053ef82378cfc32e2fbc193bc27c6a581e6c40
Jorge Henrique <jorgehenriquedasilva2014@hotmail.com>
2022-07-31 18:48:53 -0300

    flat-list-component

--- App.tsx
+++ App.tsx
@@ -1,23 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { Button } from './src/components/Button'
+import { Header } from './src/components/Header'
+import { FlatList } from './src/components/FlatList'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Git Branch Develop</Text>
-      <Button />
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.header_container}>
+        <Header />
+      </View>
+      
+      <FlatList />
+      <StatusBar style="light" backgroundColor="#0038A8" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  header_container: {
+    width: '100%',
+    height: '15%',
+    backgroundColor: '#0038A8',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text_container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
